e3ad431ae82c5d5c6abd07062ff793a580aa358c
Priyanka Chejara <pchejara1000@gmail.com>
2022-12-30 22:46:42 +0530

    start

--- hebe/src/index.js
+++ hebe/src/index.js
@@ -3,23 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {ChakraProvider} from "@chakra-ui/react"
-import {BrowserRouter} from "react-router-dom"
-import { Provider } from 'react-redux';
-import {store} from './Store/Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <BrowserRouter>
-    <ChakraProvider>
-      <Provider store={store}>
-      <App />
-      </Provider>
-    
-    </ChakraProvider>
-    </BrowserRouter>
- 
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
