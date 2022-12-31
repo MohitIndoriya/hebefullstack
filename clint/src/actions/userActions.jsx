55f7715730d832263f857e330174e35181c7b81c
MohitIndoriya <mkibap@gmail.com>
2022-12-31 16:22:19 +0530

    user done

--- clint/src/actions/userActions.jsx
+++ clint/src/actions/userActions.jsx
@@ -2,11 +2,12 @@ import axios from "axios"
 import { useNavigate } from "react-router-dom"
 
 export const Signin=(creds)=>async (dispatch)=>{
-    console.log("chalgyooooo")
+   // console.log("chalgyooooo")
     dispatch({type:"LODING"})
     try{
         let res=await axios.post(`http://localhost:8080/users/login`,creds)
         console.log(res,"ohu")
+        localStorage.setItem("token",res.data)
         if(res.data.length>0){
            
             return dispatch({type:"LOGIN",paylode:res.data[0].firstName})
